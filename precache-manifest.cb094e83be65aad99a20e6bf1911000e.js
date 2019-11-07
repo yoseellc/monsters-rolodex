bc9c08f9222184fd471dd8038949972357586f50
popelj01 <Judefusemak8>
2019-11-07 08:26:40 +0100

    Updates

---
+++ precache-manifest.cb094e83be65aad99a20e6bf1911000e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445a825d330f5f23e8199518c3d73fe0",
+    "url": "/yoseellc/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "008b0659c3c2b5182f7c",
+    "url": "/yoseellc/monsters-rolodex/static/css/main.e4da3872.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/yoseellc/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "008b0659c3c2b5182f7c",
+    "url": "/yoseellc/monsters-rolodex/static/js/main.393782e5.chunk.js"
+  },
+  {
+    "revision": "81d284521e27ad654b2f",
+    "url": "/yoseellc/monsters-rolodex/static/js/runtime-main.7606b0be.js"
+  }
+]);
